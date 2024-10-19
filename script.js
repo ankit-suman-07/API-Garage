ce1d72f1f3663b8870b2b22a3048e3cf30e418b7
ankit-suman-07 <ankitsuman07@gmail.com>
2024-10-20 01:02:34 +0530

    accessing data from html file into script file

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const queryParamsContainer = document.querySelector("[data-query-params]");
 const requestHeadersContainer = document.querySelector("[data-query-headers]");
+
+queryParamsContainer.append(createKeyValuePair())
+requestHeadersContainer.append(createKeyValuePair())
+
+function createKeyValuePair() {
+    
+}
